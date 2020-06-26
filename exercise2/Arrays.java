4edb6e0333d834d06615d3c0f40f7aa3fa812e6d
Cody Clark <codyclark2119@gmail.com>
2020-06-25 23:15:36 -0500

    completed exercise 2

--- exercise2/Arrays.java
+++ exercise2/Arrays.java
@@ -2,13 +2,30 @@ package exercise2;
 
 class Arrays {
     //Take the arrays.js file, and refactor the code into Java in this file.
-    int[] nums;
-    String[] greetings;
-    boolean[] binaries;
+    final static int[] nums = {10,23,3,4,5,2,1};
+    final static String[] greetings = {"hello","howdy","hallo","hola","bonjour","ciao","أهلا"};
+    final static boolean[] binaries = {false,true,true,true,false,false,false,true,true,true,true,true,false,true,false,false,true,false};
 
-    void printNums() {};
-    void printGreetings() {};
-    void printBinaries() {};
+    public static void printNums() {
+        for (int num : nums) {
+            System.out.println(num);
+        }
+    };
+    public static void printGreetings() {
+        for (String greet : greetings) {
+            System.out.println(greet);
+        }
+    };
+    public static void printBinaries() {
+        for (boolean binary : binaries) {
+            System.out.println(binary);
+        }
+    };
+    public static void main(String[] args){
+        printNums();
+        printGreetings();
+        printBinaries();
+    }
 }
 
 
--- exercise2/Colors.java
+++ exercise2/Colors.java
@@ -1,10 +1,25 @@
 package exercise2;
 
-public class Colors {
+import java.util.ArrayList;
 
+public class Colors {
+    String[] colorArr = {"blue", "red", "yellow", "black", "green", "pink"};
     String[] colorful(String[] strings) {
+        ArrayList<String> placeholderArr = new ArrayList<String>();
         // For the given strings, keep all the strings that begin with a color ("red", "yellow", "green", "blue", etc.) return an array of strings that start with a color.  discard all the other strings.  If no strings start with a color in the input array, return an empty array.
-        return new String[]{};
+        for(int i = 0; i< strings.length;i++){
+            for(int j = 0; j<colorArr.length; j++){
+                if(strings[i].contains(colorArr[j])){
+                    placeholderArr.add(strings[i]);
+                }
+            }
+        }
+        System.out.println(placeholderArr.toString());
+        String[] finalArr = new String[placeholderArr.size()];
+        for(int f = 0; f < placeholderArr.size(); f++){
+            finalArr[f] = placeholderArr.get(f);
+        }
+        return finalArr;
     }
 
     public static void main(String[] args) {
